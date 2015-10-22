40a7f6ad5926307f76c3ad15cbf57ca2d75d0ba3
femiosinowo <femiosinowo@gmail.com>
2015-10-22 09:29:08 -0400

    Remove Selinux profile for now

--- manifests/jenkins_master.pp
+++ manifests/jenkins_master.pp
@@ -1,9 +1,9 @@
 class profile::jenkins_master(){
 	include jenkins::master
 	
-	class { selinux:
- 	 	mode => 'enforcing'
-		}		
+	#class { selinux:
+ 	#	mode => 'enforcing'
+	#	}		
 
 	firewall { '100 allow Jenkins on 8080':
     		dport   => 8080,
