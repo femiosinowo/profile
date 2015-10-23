cfd380b2fb418b0e22e49f86e785279f948ba983
fosin <fosin@GOVCIO-92015LT2>
2015-10-23 10:10:27 -0400

    added loads

--- manifests/r10k.pp
+++ manifests/r10k.pp
@@ -15,6 +15,29 @@ class profile::r10k{
   },
 }
 
+# Internal webhooks often don't need authentication and ssl
+# Change the url below if this is changed
+class {'r10k::webhook::config':
+  enable_ssl     => false,
+  protected      => false,
+}
 
+class {'r10k::webhook':
+  require => Class['r10k::webhook::config'],
+}
+
+# https://github.com/abrader/abrader-gms
+# Add webhook to control repository ( the repo where the Puppetfile lives )
+git_webhook { 'web_post_receive_webhook' :
+  ensure       => present,
+  webhook_url  => 'http://puppet.paosin.local:81/payload',
+  token        =>  '1d2f7a01d97a24dc4fedd7d77d43fb486dc9150a',
+  project_name => 'femiosinowo/r10k',
+  server_url   => 'https://github.com/femiosinowo',
+  provider     => 'github',
+}
+
+
+ 
   
 }
