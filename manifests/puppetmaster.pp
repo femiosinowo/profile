7612cb34c3fd8995cc248882620f66bd6be98a83
femi osinowo <femi@paosin.local>
2015-10-26 09:55:47 -0400

    modified the comma

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -21,7 +21,7 @@ class profile::puppetmaster{
     class { selinux: mode => 'disabled' }
 
   firewall { '100 allow puppet stuff':
-    dport  => [443,61613,8140]
+    dport  => [443,61613,8140],
     proto  => tcp,
     action => accept,
   }
