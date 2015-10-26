1678b719e2d7470bb52f844c2bb4d0e726c05a3f
femi osinowo <femi@paosin.local>
2015-10-26 15:40:00 -0400

    added it

--- manifests/wordpress.pp
+++ manifests/wordpress.pp
@@ -1,1 +1,33 @@
+ class profile::wordpress {
+  # # Configure apache
+  include apache
+  include apache::mod::php
+  include apache::mod::ssl
+
+  # # Configure mysql
+  include '::mysql::server'
+
+  class { '::mysql::client': bindings_enable => true, }
+
+  # # Configure wordpress
+  class { '::wordpress':
+    install_dir => '/var/www/html/paosin',
+    db_name     => 'paosin',
+    db_host     => 'mysql.paosin.local',
+    db_password => 'P@ssw0rd',
+  }
+
+  # class { 'wordpress': }
+
+  class { selinux:
+    mode => 'permissive'
+  }
+
+  firewall { '100 allow Port 80':
+    dport  => [80, 443],
+    proto  => tcp,
+    action => accept,
+  }
+
+}
  
