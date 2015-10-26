bfd9beff770667a7e59411a45a3fcb2a3dc317e9
femi osinowo <femi@paosin.local>
2015-10-26 15:15:35 -0400

    added some bindings agagin

--- manifests/wordpresss.pp
+++ manifests/wordpresss.pp
@@ -1,27 +1,24 @@
 class profile::wordpresss {
+  class { 'mysql::client': bindings_enable => true, }
 
+  class { 'mysql::bindings':}
 
- 
-class { 'mysql::client':
-    bindings_enable => true,
+  class { 'wordpress':
+    db_user     => 'femi',
+    db_password => 'P@ssw0rd',
+    db_host     => 'mysql.paosin.local',
   }
 
-class { 'wordpress':
-  db_user     => 'femi',
-  db_password => 'P@ssw0rd',
-  db_host     => 'mysql.paosin.local',
-}
+  # class { 'wordpress': }
 
-#class { 'wordpress': }
-
-  class { selinux: mode => 'permissive' }
+  class { selinux:
+    mode => 'permissive'
+  }
 
   firewall { '100 allow Port 80':
-    dport  => [80,443],
+    dport  => [80, 443],
     proto  => tcp,
     action => accept,
   }
 
-
-  
 }
