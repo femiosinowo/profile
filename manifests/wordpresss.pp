cf2c9749e0f2e1fff569d7204889809e82b5a24a
femi osinowo <femi@paosin.local>
2015-10-26 13:46:43 -0400

    test

---
+++ manifests/wordpresss.pp
@@ -0,0 +1,22 @@
+class profile::wordpresss {
+
+
+class { 'wordpress':
+  db_user     => 'femi',
+  db_password => 'P@ssw0rd',
+  db_host     => 'mysql.paosin.local',
+}
+
+#class { 'wordpress': }
+
+  class { selinux: mode => 'permissive' }
+
+  firewall { '100 allow Port 80':
+    dport  => [80,443],
+    proto  => tcp,
+    action => accept,
+  }
+
+
+ 
+}
