e17b3833625a96027e9fc15b34f4b653e4db68e0
femi osinowo <femi@paosin.local>
2015-10-26 13:38:41 -0400

    aa

--- manifests/wordpress.pp
+++ manifests/wordpress.pp
@@ -1,12 +1,8 @@
 class profile::wordpress {
 
 
-class { 'wordpress':
-  db_user     => 'femi',
-  db_password => 'P@ssw0rd',
-  db_host     => 'mysql.paosin.local',
-}
-
+ 
+class { 'wordpress': }
 
   class { selinux: mode => 'permissive' }
 
