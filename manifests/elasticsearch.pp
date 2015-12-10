5cb962f1d58826faedcc3f3f13ee40141422ca28
femi osinowo <femi@paosin.local>
2015-12-10 15:31:11 -0500

    some quotations

--- manifests/elasticsearch.pp
+++ manifests/elasticsearch.pp
@@ -1,9 +1,9 @@
 class profile::elasticsearch () {
   class { '::elasticsearch':
     java_install => true,
-    ensure       => present,
+    ensure       => 'present',
     manage_repo  => true,
-    repo_version => '1.4',
+    repo_version => '2.1',
   # package_url  =>
   # 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.1.0/elasticsearch-2.1.0.rpm'
   }
