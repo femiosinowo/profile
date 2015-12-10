56d11ffd908b371c381bdf826b70151a21dcf3d4
femi osinowo <femi@paosin.local>
2015-12-10 13:51:46 -0500

    asdf

--- manifests/logstash.pp
+++ manifests/logstash.pp
@@ -8,13 +8,14 @@ class profile::logstash () {
   class { '::logstash':
     ensure       => present,
     java_install => true,
-    before       => Exec['create_certs'],
+    #before       => Exec['create_certs'],
+    exec{'openssl req -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout /etc/pki/tls/private/logstash-forwarder.key -out /etc/pki/tls/certs/logstash-forwarder.crt'}
   }
 
-  exec { 'create_certs':
-    require => File['cert_dir'],
-    command => 'openssl req -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout /etc/pki/tls/private/logstash-forwarder.key -out /etc/pki/tls/certs/logstash-forwarder.crt',
-  }
+  #exec { 'create_certs':
+  #  require => File['cert_dir'],
+  #  command => 'openssl req -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout /etc/pki/tls/private/logstash-forwarder.key -out /etc/pki/tls/certs/logstash-forwarder.crt',
+  #}
 
   logstash::configfile { '/etc/logstash/conf.d/config.conf': content => template('profile/logstash/config.conf.erb'), }
 
