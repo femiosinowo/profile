9e8bd94b3e76caa54d8e8bffc0f881f6fcbfb118
femi osinowo <femi@paosin.local>
2015-12-10 13:08:08 -0500

    added log stash stuff

--- manifests/logstash.pp
+++ manifests/logstash.pp
@@ -1,8 +1,15 @@
 class profile::logstash(){
   
-  class { 'logstash': }
-  logstash::configfile { 'configname':
-  content => template('path/to/config.file')
+  class { 'logstash':
+    ensure        => present,
+    java_install  => true,
+    exec{'mkdir -p /etc/pki/tls'}
+    exec{'openssl req -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout /etc/pki/tls/private/logstash-forwarder.key -out /etc/pki/tls/certs/logstash-forwarder.crt'}
+  }
+  
+
+ logstash::configfile { '/etc/logstash/conf.d/config.conf':
+  content => template('profile/logstash/config.conf.erb')
 }
 
 }
