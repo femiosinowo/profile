81a0519e5f1d6403c7a77c7d422166babec5d00d
femi osinowo <femi@paosin.local>
2015-12-10 14:04:14 -0500

    added manage_repo for elastic search

--- manifests/elasticsearch.pp
+++ manifests/elasticsearch.pp
@@ -1,7 +1,11 @@
 class profile::elasticsearch () {
-  class { '::elasticsearch': java_install => true }
+  class { '::elasticsearch':
+    java_install => true,
+    manage_repo  => true,
+  # package_url       =>
+  # 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.1.0/elasticsearch-2.1.0.rpm'
+  }
 
-  
   elasticsearch::instance { 'es-01': }
 
 }
