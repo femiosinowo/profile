6921185233a49dc0e6791d5eff6305f699a51290
femi osinowo <femi@paosin.local>
2015-12-10 13:33:01 -0500

    re-arranged the metaparameters

--- manifests/logstash.pp
+++ manifests/logstash.pp
@@ -1,19 +1,20 @@
 class profile::logstash () {
+  
   file { '/etc/pki/tls':
     ensure => directory,
     group  => root,
     alias  => 'cert_dir'
   }
 
-  class { 'logstash':
-    require      => File['cert_dir'],
+  class { 'logstash':    
     ensure       => present,
     java_install => true,
-    require      => Exec['create_certs']
+    before      => Exec['create_certs'],
+    
   }
 
   exec { 'create_certs':
-    path    => 'C:/Windows/System32/WindowsPowerShell/v1.0',
+    require      => File['cert_dir'],
     command => 'openssl req -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout /etc/pki/tls/private/logstash-forwarder.key -out /etc/pki/tls/certs/logstash-forwarder.crt',
   }
 
