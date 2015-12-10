74289d52662c1dd1db88b8e45dd8710a2d6038b0
femi osinowo <femi@paosin.local>
2015-12-10 14:05:19 -0500

    manage_repo to true

--- manifests/logstash.pp
+++ manifests/logstash.pp
@@ -9,14 +9,14 @@ class profile::logstash () {
     ensure       => present,
     java_install => true,
     before       => Exec['create_certs'],
-    package_url => 'https://download.elastic.co/logstash/logstash/packages/centos/logstash-2.1.1-1.noarch.rpm',
-     
+    manage_repo  => true,
+  # package_url => 'https://download.elastic.co/logstash/logstash/packages/centos/logstash-2.1.1-1.noarch.rpm',
   }
 
   exec { 'create_certs':
     require => File['cert_dir'],
-     path   => "/usr/bin:/usr/sbin:/bin",
-     command => 'openssl req -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout /etc/pki/tls/private/logstash-forwarder.key -out /etc/pki/tls/certs/logstash-forwarder.crt',
+    path    => "/usr/bin:/usr/sbin:/bin",
+    command => 'openssl req -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout /etc/pki/tls/private/logstash-forwarder.key -out /etc/pki/tls/certs/logstash-forwarder.crt',
   }
 
   logstash::configfile { '/etc/logstash/conf.d/config.conf': content => template('profile/logstash/config.conf.erb'), }
