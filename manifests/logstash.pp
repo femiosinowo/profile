eb647927f5f5a77b695389a207d7ccb1a2440196
femi osinowo <femi@paosin.local>
2015-12-10 13:21:33 -0500

    ab

--- manifests/logstash.pp
+++ manifests/logstash.pp
@@ -1,15 +1,23 @@
 class profile::logstash(){
   
+  file { '/etc/pki/tls':
+  ensure  => directory,
+  group   => root,
+  alias    => 'cert_dir'
+}
+
+
   class { 'logstash':
+    require       => File['cert_dir'],
     ensure        => present,
-    java_install  => true,
-    exec{'mkdir -p /etc/pki/tls'}
-    exec{'openssl req -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout /etc/pki/tls/private/logstash-forwarder.key -out /etc/pki/tls/certs/logstash-forwarder.crt'}
+    java_install  => true,     
+    exec{'openssl req -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout /etc/pki/tls/private/logstash-forwarder.key -out /etc/pki/tls/certs/logstash-forwarder.crt'},
   }
+  
   
 
  logstash::configfile { '/etc/logstash/conf.d/config.conf':
-  content => template('profile/logstash/config.conf.erb')
+  content => template('profile/logstash/config.conf.erb'),
 }
 
 }
