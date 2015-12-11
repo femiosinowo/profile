47e490a6e7cf29420316701a60415f8748dea88a
femi osinowo <femi@paosin.local>
2015-12-11 13:13:42 -0500

    added what filebeat will monitor and ship to logstash

--- manifests/filebeat.pp
+++ manifests/filebeat.pp
@@ -1,9 +1,19 @@
 class profile::filebeat () {
   class { '::filebeat':
-    outputs => {
+    outputs  => {
       'logstash' => {
         'hosts'       => ['elk-stack:5044'],
         'loadbalance' => true,
+        # 'tls'
+      }
+      ,
+    }
+    ,
+    filebeat => {
+      'prospectors' => {
+        'paths'         => ['/var/log/*.log', '/var/log/syslog', '/var/log/auth.log'],
+        'document_type' => 'syslog',
+        'input_type'    => 'log',
       }
       ,
     }
