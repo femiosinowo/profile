d6d4a3bc08b65fb2f1bf4910244f9730d1cefddd
Femi From Dell <Femi@paosin.com>
2015-12-10 21:07:43 -0500

    changed kibana

--- manifests/kibana.pp
+++ manifests/kibana.pp
@@ -1,4 +1,20 @@
-class profile::kibana(){
-  
-  class { '::kibana': }
+class profile::kibana () {
+  # class { '::kibana': }
+
+  class { '::kibana4':
+    package_ensure   => '4.3.0-linux-x64',
+    package_provider => 'archive',
+    symlink          => true,
+    manage_user      => true,
+    kibana4_user     => kibana4,
+    kibana4_group    => kibana4,
+    kibana4_gid      => 200,
+    kibana4_uid      => 200,
+    config           => {
+      'server.port'       => 5601,
+      'server.host'       => '0.0.0.0',
+      'elasticsearch.url' => 'http://localhost:9200',
+    }
+  }
+
 }
