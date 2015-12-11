6bf5a3511d5160fb6c4f02744442d31349ef2374
femi osinowo <femi@paosin.local>
2015-12-11 12:54:19 -0500

    stuff

--- manifests/filebeat.pp
+++ manifests/filebeat.pp
@@ -1,14 +1,13 @@
-class profile::filebeat(){
-  
-  class { 'filebeat':
-  outputs => {
-    'logstash'     => {
-     'hosts' => [
-       'elk-stack:5044'
-     ],
-     'loadbalance' => true,
-    },
-  },
-}
-  
+class profile::filebeat () {
+  class { '::filebeat':
+    outputs => {
+      'logstash' => {
+        'hosts'       => ['elk-stack:5044'],
+        'loadbalance' => true,
+      }
+      ,
+    }
+    ,
+  }
+
 }
