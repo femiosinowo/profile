0adf706072d2654ff1d9d32f66265fd11460be58
Femi From Dell <Femi@paosin.com>
2015-12-10 20:55:21 -0500

    whatever

--- manifests/elasticsearch.pp
+++ manifests/elasticsearch.pp
@@ -3,7 +3,7 @@ class profile::elasticsearch () {
     java_install => true,
     ensure       => 'present',
      manage_repo  => true,
-     repo_version => '1.4',
+     repo_version => 'latest',
     #package_url  => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.1.0/elasticsearch-2.1.0.rpm',
     #autoupgrade  => true,
   }
