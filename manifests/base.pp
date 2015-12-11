86068909e09e7743400a7a2229bea03d20910666
femi osinowo <femi@paosin.local>
2015-12-11 12:46:59 -0500

    added filebeat to it

--- manifests/base.pp
+++ manifests/base.pp
@@ -5,26 +5,21 @@ class profile::base {
   include resolver
   include epel
   # include selinux
-  #include vmwaretools
-#ntp module
-#class { '::ntp':
-#  servers => [ '0.us.pool.ntp.org', '1.us.pool.ntp.org' ],
-#}
+  # include vmwaretools
+  # ntp module
+  # class { '::ntp':
+  #  servers => [ '0.us.pool.ntp.org', '1.us.pool.ntp.org' ],
+  #}
 
 
-  
   # base firewall config
-    include profile::firewall 
-  
-  
+  include profile::firewall
+
   # common packages needed everywhere
-    package {[
-            'tree',
-            'sudo',
-            'screen'
-        ]:
-        ensure => present,
-    }
+  package { ['tree', 'sudo', 'screen']: ensure => present, }
+
+  # file beat for log shipping
+  include profile::filebeat
 
 }
 
