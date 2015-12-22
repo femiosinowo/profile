a0c2cc9c5f2998c4aac9422bbb6d0efdfcdcc4a1
femi osinowo <femi@paosin.local>
2015-12-22 14:45:26 -0500

    asdf

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,10 +1,10 @@
 class profile::puppetmaster {
   class { selinux: mode => 'disabled' }
 
-  firewall { '100 allow puppet stuff':
-    dport  => [443, 61613, 8140],
-    proto  => tcp,
-    action => accept,
-  }
+#  firewall { '100 allow puppet stuff':
+#    dport  => [443, 61613, 8140],
+#    proto  => tcp,
+#    action => accept,
+#  }
 
 }
