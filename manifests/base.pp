9413c7a935b9205fef4923529dceac3b923f7728
femi osinowo <femi@paosin.local>
2015-12-23 11:54:01 -0500

    added ntp

--- manifests/base.pp
+++ manifests/base.pp
@@ -20,6 +20,11 @@ class profile::base {
 
   # file beat for log shipping
   #include profile::filebeat
+  
+  class { '::ntp':
+    servers => [ '0.pool.ntp.org', '2.centos.pool.ntp.org', '1.rhel.pool.ntp.org'],
+  }
+  
 
 }
 
--- manifests/r10k_p.pp
+++ manifests/r10k_p.pp
@@ -32,55 +32,17 @@ class profile::r10k_p {
 
   # https://github.com/abrader/abrader-gms
   # Add webhook to control repository ( the repo where the Puppetfile lives )
-  git_webhook { 'web_post_receive_webhook':
-    ensure       => present,
-    webhook_url  => 'http://puppet:puppet@98.233.241.211:8088/payload',
-    token        => hiera('github_api_token'),
-    project_name => 'femiosinowo/r10k',
-    server_url   => 'https://api.github.com',
-    provider     => 'github',
-    disable_ssl_verify => true,
-  }
-
-  #  class { 'r10k::webhook::config':
-  #    use_mcollective  => false,
-  #    public_key_path  => '/etc/puppetlabs/mcollective/server_public.pem', # Mandatory for FOSS
-  #    private_key_path => '/etc/puppetlabs/mcollective/server_private.pem', # Mandatory for FOSS
-  #  }
-  #
-  #  class { 'r10k::webhook':
-  #    user  => 'root',
-  #    group => '0',
-  #  }
-  #  Package['mcollective-common'] -> Class['r10k::webhook']
-  #  Class['r10k::webhook::config'] -> Class['r10k::webhook']
-
-
-  # class { 'webhook': }
-
-
-  # External webhooks often need authentication and ssl and authentication
-  # Change the url below if this is changed
-  # class {'r10k::webhook::config':
-  #  enable_ssl => true,
-  #  protected  => true,
-  #  notify     => Service['webhook'],
-  #}
-  #
-  # class {'r10k::webhook':
-  #  require => Class['r10k::webhook::config'],
-  #}
-  #
   # # https://github.com/abrader/abrader-gms
   # # Add webhook to control repository ( the repo where the Puppetfile lives )
   # # Requires gms 0.0.6+ for disable_ssl_verify param
-  # git_webhook { 'web_post_receive_webhook' :
-  #  ensure             => present,
-  #  webhook_url        => 'http://puppet.paosin.local:81/payload',
-  #  token              =>  '1d2f7a01d97a24dc4fedd7d77d43fb486dc9150a',
-  #  project_name       => 'femiosinowo/r10k',
-  #  server_url         => 'https://api.github.com',
-  #  disable_ssl_verify => true,
-  #  provider           => 'github',
-  #}
+  git_webhook { 'web_post_receive_webhook':
+    ensure             => present,
+    webhook_url        => 'http://puppet:puppet@98.233.241.211:8088/payload',
+    token              => hiera('github_api_token'),
+    project_name       => 'femiosinowo/r10k',
+    server_url         => 'https://api.github.com',
+    provider           => 'github',
+    disable_ssl_verify => true,
+  }
+
 }
