ae25d150b71ed43cc6645eee594920bc3b3ae751
Femi From Dell <Femi@paosin.com>
2015-12-29 15:04:45 -0500

    correction

--- manifests/network.pp
+++ manifests/network.pp
@@ -2,11 +2,11 @@ class profile::network () {
   network::if::static { 'eth0':
     ensure    => 'up',
     ipaddress => hiera('network::ipaddress'),
+    netmask   => hiera('network::netmask'),
   }
 
   class { 'network::global':
     gateway => hiera('network::gateway_addr'),
-    netmask => hiera('network::netmask'),
   }
 
 }
