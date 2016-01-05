f1eb26461c4ddd05ed95c3f9254febb8d2498e86
Femi From Dell <Femi@paosin.com>
2016-01-05 10:47:06 -0500

    asb

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,11 +1,11 @@
 class profile::puppetmaster {
   class { selinux: mode => 'disabled' }
 
-  #  firewall { '100 allow puppet stuff':
-  #    dport  => [443, 61613, 8140],
-  #    proto  => tcp,
-  #    action => accept,
-  #  }
+#  firewall { '120 allow puppet stuff':
+#    dport  => [22, 443, 61613, 8140, 8088],
+#    proto  => tcp,
+#    action => accept,
+#  }
 
   package { 'puppet-lint':
     ensure   => '1.1.0',
