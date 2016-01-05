3a763126eb95fcca9138086e5142cbb8ecbdb41e
Femi From Dell <Femi@paosin.com>
2016-01-05 09:42:43 -0500

    removed network for now

--- manifests/network_p.pp
+++ manifests/network_p.pp
@@ -1,10 +1,10 @@
 class profile::network_p () {
-  class { 'network::global': gateway => hiera('network::gateway_addr'), }
-
-  network::if::static { 'eth0':
-    ensure    => 'up',
-    ipaddress => hiera('network::ipaddress'),
-    netmask   => hiera('network::netmask'),
-  }
+#  class { 'network::global': gateway => hiera('network::gateway_addr'), }
+#
+#  network::if::static { 'eth0':
+#    ensure    => 'up',
+#    ipaddress => hiera('network::ipaddress'),
+#    netmask   => hiera('network::netmask'),
+#  }
 
 }
