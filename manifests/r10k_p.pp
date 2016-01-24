b5473be1577a0ca1695d2cdbc9c5463605fe7540
Femi From Dell <Femi@paosin.com>
2016-01-24 14:02:36 -0500

    removed webhooks for PE

--- manifests/r10k_p.pp
+++ manifests/r10k_p.pp
@@ -16,34 +16,36 @@ class profile::r10k_p {
     ,
   }
 
+#### Webhooks removed cos it is not PE ###########################
+
   # Required unless you disable mcollective
   #include r10k::mcollective
 
   # Internal webhooks often don't need authentication and ssl
   # Change the url below if this is changed
-  class { 'r10k::webhook::config':
-    enable_ssl => false,
-    protected  => false,
-  }
+#  class { 'r10k::webhook::config':
+#    enable_ssl => false,
+#    protected  => false,
+#  }
 
-  class { 'r10k::webhook':
-    require => Class['r10k::webhook::config'],
-  }
+#  class { 'r10k::webhook':
+#    require => Class['r10k::webhook::config'],
+#  }
 
   # https://github.com/abrader/abrader-gms
   # Add webhook to control repository ( the repo where the Puppetfile lives )
   # # https://github.com/abrader/abrader-gms
   # # Add webhook to control repository ( the repo where the Puppetfile lives )
   # # Requires gms 0.0.6+ for disable_ssl_verify param
-  git_webhook { 'web_post_receive_webhook':
-    ensure             => present,
-    webhook_url        => 'http://puppet:puppet@98.233.243.40:8088/payload',
-    token              => hiera('github_api_token'),
-    project_name       => 'femiosinowo/r10k',
-    server_url         => 'https://api.github.com',
-    provider           => 'github',
-    disable_ssl_verify => true,
-  }
+#  git_webhook { 'web_post_receive_webhook':
+#    ensure             => present,
+#    webhook_url        => 'http://puppet:puppet@98.233.243.40:8088/payload',
+#    token              => hiera('github_api_token'),
+#    project_name       => 'femiosinowo/r10k',
+#    server_url         => 'https://api.github.com',
+#    provider           => 'github',
+#    disable_ssl_verify => true,
+#  }
   
   
   
