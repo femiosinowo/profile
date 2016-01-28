8ead6c28352a151d392b89c94fabfc17b71b1736
Femi From Dell <Femi@paosin.com>
2016-01-28 10:12:32 -0500

    added nexus

--- manifests/artifacts.pp
+++ manifests/artifacts.pp
@@ -1,4 +1,16 @@
 class profile::artifacts () {
-  class { 'artifactory': serverAlias => ['artifactory', 'artifactory.paosin.local'] }
+  # class { 'artifactory': serverAlias => ['artifactory', 'artifactory.paosin.local'] }
+  # puppetlabs-java
+  # NOTE: Nexus requires
+  class { '::java':
+  }
 
+  class { '::nexus':
+    version    => '2.8.0',
+    revision   => '05',
+    nexus_root => '/srv', # All directories and files will be relative to this
+  }
+
+  Class['::java'] ->
+  Class['::nexus']
 }
