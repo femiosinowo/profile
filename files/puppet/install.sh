5c369d45732cd729731209de4d4257c84f300d7e
Femi From Dell <Femi@paosin.com>
2016-02-03 16:10:36 -0500

    updated ntpd

--- files/puppet/install.sh
+++ files/puppet/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 yum install ntp -y
+service ntpd start
+chkconfig ntpd on
+ 
 rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
 yum --enablerepo=puppetlabs-devel install puppet -y
 
@@ -7,10 +10,10 @@ puppet config set server puppet1.paosin.local --section agent
 puppet config set environment production --section agent
 
 service puppet start
-service ntpd start
+
 
  chkconfig puppet on
- chkconfig ntpd on
+
  
  #run puppet
  puppet agent -t
