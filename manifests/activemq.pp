a89653f8c4ad8cc9b3f8dad861c456c5abc0b67e
Femi From Dell <Femi@paosin.com>
2016-02-03 15:45:25 -0500

    another activemq

--- manifests/activemq.pp
+++ manifests/activemq.pp
@@ -1,14 +1,22 @@
 class profile::activemq () {
-  class { 'java': distribution => 'jre', } ->
-  class { 'epel': } ->
-  yumrepo { 'puppetlabs-deps':
-    descr    => 'Puppet Labs Dependencies El 6 - $basearch',
-    baseurl  => 'http://yum.puppetlabs.com/el/6/dependencies/$basearch',
-    gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
-    enabled  => 1,
-    gpgcheck => 1,
+  #  class { 'java': distribution => 'jre', } ->
+  #  class { 'epel': } ->
+  #  yumrepo { 'puppetlabs-deps':
+  #    descr    => 'Puppet Labs Dependencies El 6 - $basearch',
+  #    baseurl  => 'http://yum.puppetlabs.com/el/6/dependencies/$basearch',
+  #    gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
+  #    enabled  => 1,
+  #    gpgcheck => 1,
+  #  } ->
+  #  class { '::activemq': package_type => 'rpm', }
+  #
+  #  class { '::activemq::stomp': port => 61613, }
+  notify { 'alpha': } ->
+  class { 'java':
+    distribution => 'jdk',
+    version      => 'latest',
   } ->
-  class { '::activemq': package_type => 'rpm', }
+  class { '::activemq': webconsole => false, } ->
+  notify { 'omega': }
 
-  class { '::activemq::stomp': port => 61613, }
 }
