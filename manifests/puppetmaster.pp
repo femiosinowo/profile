a973e766afa438d7b80eea42d2a7e2d989553c34
Femi From Dell <Femi@paosin.com>
2016-02-05 14:35:18 -0500

    moved site.pp to git

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -7,8 +7,13 @@ class profile::puppetmaster ($brokerHost = hiera('mcollective::brokerhost')) {
     mode   => 'u+x'
   }
 
+  file { '/etc/puppet/environments/production/manifests/site.pp':
+    ensure => file,
+    source => "puppet:///modules/profile/puppet/site.pp",
+  }
+
   firewall { '120 allow puppet stuff':
-    dport  => [80, 443, 61613,61614, 8140, 8088],
+    dport  => [80, 443, 61613, 61614, 8140, 8088],
     proto  => tcp,
     action => accept,
   }
@@ -19,25 +24,24 @@ class profile::puppetmaster ($brokerHost = hiera('mcollective::brokerhost')) {
   }
 
   Package {
-    allow_virtual => false }
+    allow_virtual => false, }
 
- 
-#  class { '::mcollective':
-#    broker_host       => $brokerHost,
-#    broker_port       => '61614',
-#    security_provider => 'psk',
-#    security_secret   => 'P@ssw0rd',
-#    use_node          => false,
-#  }
-#  include ::mcollective::client
-  
-#  class { 'mcollective':
-#  stomp_host     =>$brokerHost,
-#  stomp_user     => 'mcollective',
-#  stomp_password => 'P@ssw0rdP@ssw0rd',
-#  client         => true,
-#  client_group   => 'mco-users',
-#}
+  #  class { '::mcollective':
+  #    broker_host       => $brokerHost,
+  #    broker_port       => '61614',
+  #    security_provider => 'psk',
+  #    security_secret   => 'P@ssw0rd',
+  #    use_node          => false,
+  #  }
+  #  include ::mcollective::client
+
+  #  class { 'mcollective':
+  #  stomp_host     =>$brokerHost,
+  #  stomp_user     => 'mcollective',
+  #  stomp_password => 'P@ssw0rdP@ssw0rd',
+  #  client         => true,
+  #  client_group   => 'mco-users',
+  #}
 
 
   #    class { 'hiera':
