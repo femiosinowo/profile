a18a8d4f2500aee579cd41835a24483a7673b2b4
Femi From Dell <Femi@paosin.com>
2016-02-05 14:19:11 -0500

    added mcollective

--- manifests/base.pp
+++ manifests/base.pp
@@ -23,6 +23,14 @@ class profile::base ($brokerHost = hiera('mcollective::brokerhost') ) {
     proto  => tcp,
     action => accept,
   }
+  
+  file{"/etc/mcollective/facts.yaml":
+      owner    => root,
+      group    => root,
+      mode     => 400,
+      loglevel => debug, # reduce noise in Puppet reports
+      content  => inline_template("<%= scope.to_hash.reject { |k,v| k.to_s =~ /(uptime_seconds|timestamp|free)/ }.to_yaml %>"), # exclude rapidly changing facts
+    }
 
 }
 
