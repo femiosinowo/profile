9d4953fbf03a0fc0ec7367db9b015bb02677525a
Femi From Dell <Femi@paosin.com>
2016-02-06 12:53:40 -0500

    added this stuff

--- manifests/mco-client.pp
+++ manifests/mco-client.pp
@@ -1,5 +1,5 @@
 class profile::mco-client () {
 
   mcollective::plugin::agent { 'puppet': version => latest, }
-  #mcollective::plugin::client { 'puppet': version => latest, }  
+  mcollective::plugin::client { 'puppet': version => latest, }  
 }
