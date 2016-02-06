6e2c9feca7ef3043b1ccbb1a017e8c8335508d37
Femi From Dell <Femi@paosin.com>
2016-02-06 14:07:46 -0500

    awfef

--- files/puppet/site.pp
+++ files/puppet/site.pp
@@ -12,6 +12,14 @@ hiera_include('classes')
 }
 node 'puppet1.paosin.local'{
 include role::puppetmaster
+
+class { '::mcollective':
+hosts => ['activemq.paosin.local'],
+client_password => 'P@ssw0rdP@ssw0rd',
+server_password => 'P@ssw0rdP@ssw0rd',
+psk_key => 'f525ae7c92fca724b06ed6701cf99ca255e992f9a057084ddbfb75c73dc9c4d9',
+}
+
 # Every node installs the server
 include mcollective::server
 include mcollective::client
