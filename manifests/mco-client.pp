b95bb10b3621861c27614c046fe6c9cb746f135f
Femi From Dell <Femi@paosin.com>
2016-02-06 12:01:38 -0500

    mcollective::plugin::agent { 'puppet': } # for servers mcollective::plugin::client { 'puppet': } # for clients

--- manifests/mco-client.pp
+++ manifests/mco-client.pp
@@ -1,7 +1,5 @@
 class profile::mco-client () {
+  mcollective::plugin::agent { 'puppet': } # for servers
 
-  mcollective::plugin::agent { 'puppet':
-    version      => latest,
-
-  }
+  mcollective::plugin::client { 'puppet': } # for clients
 }
