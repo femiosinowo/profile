965fe27a5c52229f72ed55f76c9194549b74fe12
Femi From Dell <Femi@paosin.com>
2016-02-05 20:11:44 -0500

    removed facts.yaml

--- files/puppet/site.pp
+++ files/puppet/site.pp
@@ -1,13 +1,13 @@
 node default {
 hiera_include('classes')
 
-file{"/etc/mcollective/facts.yaml":
-      owner    => root,
-      group    => root,
-      mode     => 400,
-      loglevel => debug, # reduce noise in Puppet reports
-      content  => inline_template("<%= scope.to_hash.reject { |k,v| k.to_s =~ /(uptime_seconds|timestamp|free)/ }.to_yaml %>"), # exclude rapidly changing facts
-    }
+#file{"/etc/mcollective/facts.yaml":
+#      owner    => root,
+#      group    => root,
+#      mode     => 400,
+#      loglevel => debug, # reduce noise in Puppet reports
+#      content  => inline_template("<%= scope.to_hash.reject { |k,v| k.to_s =~ /(uptime_seconds|timestamp|free)/ }.to_yaml %>"), # exclude rapidly changing facts
+#    }
 
 }
 node 'puppet1.paosin.local'{
