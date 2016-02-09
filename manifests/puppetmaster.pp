e0f80378450f1da9dfc425046675e24980f0a576
Femi From Dell <Femi@paosin.com>
2016-02-08 19:06:13 -0500

    ergw

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -23,13 +23,13 @@ class { 'hiera':
   
   mcollective::plugin::client { 'filemgr': }
 
-  mcollective::plugin::client { 'nettest': }
+  #mcollective::plugin::client { 'nettest': }
 
   mcollective::plugin::client { 'package': }
 
   mcollective::plugin::client { 'service': }
 
-  #mcollective::plugin::client { 'puppet': }
+  mcollective::plugin::client { 'puppet': }
   
   firewall { '120 allow puppet stuff':
     dport  => [80, 443, 61613, 61614, 8140, 8088],
