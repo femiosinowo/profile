5fda13ee3e8dd5732d1f8bc5db487f6a2b58e4fa
Femi-fromPuppet <femiosinowo@paosin.com>
2016-04-08 15:58:53 -0400

    added changes

--- manifests/weblogic.pp
+++ manifests/weblogic.pp
@@ -1,6 +1,10 @@
 class profile::weblogic(){
- 
- 
+ #
+ exec { 'wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "https://edelivery.oracle.com/otn-pub/java/jdk/7u25-b15/jdk-7u25-linux-x64.tar.gz"':
+  cwd     => "/etc/puppet/environments/production/modules/profile/files/",
+  #creates => "/var/tmp/myfile",
+  path    => ["/usr/bin", "/usr/sbin"]
+}
  #jdk
  include jdk7
  jdk7::install7{ 'jdk1.7.0_25':
@@ -11,7 +15,7 @@ class profile::weblogic(){
   x64                   => true,
   download_dir          => "/install",
   urandom_java_fix      => false,
-  source_path           => "/root/software",
+  source_path           => "puppet:///modules/profile/",
   default_links         => true,
   install_alternatives  => false,
 }
