50fdf3020c3217d916a1b7c179c2f1079c4c84d5
Femi-fromPuppet <femiosinowo@paosin.com>
2016-04-08 15:12:12 -0400

    added stuff

--- manifests/weblogic.pp
+++ manifests/weblogic.pp
@@ -3,15 +3,15 @@ class profile::weblogic(){
  
  #jdk
  include jdk7
- jdk7::install7{ 'jdk1.7.0_40':
-  version               => "7u40" ,
-  full_version          => "jdk1.7.0_40",
+ jdk7::install7{ 'jdk1.7.0_79':
+  version               => "7u79" ,
+  full_version          => "jdk1.7.0_79",
   java_homes            => '/usr/java/',
   alternatives_priority => 18000,
   x64                   => true,
   download_dir          => "/install",
   urandom_java_fix      => false,
-  source_path           => "puppet:///profile/jdk7/",
+  source_path           => "puppet:///modules/profile/",
   default_links         => true,
   install_alternatives  => false,
 }
