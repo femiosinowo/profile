84c33239224cf636cf315ba8263ab0b1a4bc8823
Femi-fromPuppet <femiosinowo@paosin.com>
2016-04-10 23:37:40 -0400

    added some gem

--- manifests/prof_sensu.pp
+++ manifests/prof_sensu.pp
@@ -1,24 +1,45 @@
 class profile::prof_sensu () {
   
-   class { 'sensu':
-    rabbitmq_password => 'correct-horse-battery-staple',
-    server            => true,
-    api               => true,
-    plugins           => [
-      'puppet:///data/sensu/plugins/ntp.rb',
-      'puppet:///data/sensu/plugins/postfix.rb'
-    ]
-  }
+#   class { 'sensu':
+#    rabbitmq_password => 'correct-horse-battery-staple',
+#    server            => true,
+#    api               => true,
+#    plugins           => [
+#      'puppet:///data/sensu/plugins/ntp.rb',
+#      'puppet:///data/sensu/plugins/postfix.rb'
+#    ]
+#  }
+#
+#  sensu::handler { 'default':
+#    command => 'mail -s \'sensu alert\' femi@paosin.local',
+#  }
+#
+#  sensu::check { 'check_ntp':
+#    command     => 'PATH=$PATH:/usr/lib/nagios/plugins check_ntp_time -H pool.ntp.org -w 30 -c 60',
+#    handlers    => 'default',
+#    subscribers => 'sensu-test'
+#  }
+  
+  
+  
+  package { 'sensu-plugins-disk-checks':
+  ensure   => '0.0.1',
+  provider => sensu_gem,
+}
 
-  sensu::handler { 'default':
-    command => 'mail -s \'sensu alert\' ops@foo.com',
-  }
+  package { 'sensu-plugins-ntp':
+  ensure   => 'latest',
+  provider => sensu_gem,
+}
 
-  sensu::check { 'check_ntp':
-    command     => 'PATH=$PATH:/usr/lib/nagios/plugins check_ntp_time -H pool.ntp.org -w 30 -c 60',
-    handlers    => 'default',
-    subscribers => 'sensu-test'
+  
+  
+    firewall { '101 allow 80,3306,443':
+    dport  => [3000],
+    proto  => tcp,
+    action => accept,
   }
+  
   
   
   }
