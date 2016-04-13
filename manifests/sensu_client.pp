8a6e4bc340c715c58a4c61ffb4cbc10f70936236
Femi From Dell <Femi@paosin.com>
2016-04-13 15:51:39 -0400

    some stunts on sensu

--- manifests/sensu_client.pp
+++ manifests/sensu_client.pp
@@ -1,11 +1,13 @@
-class profile::sensu_client(){
-
+class profile::sensu_client () {
   class { 'sensu':
+    server            => false,
+    api               => true,
+    rabbitmq_user     => 'sensu',
     rabbitmq_password => 'sensu',
-    rabbitmq_host => '10.0.0.58',
-    subscriptions => 'sensu-test',
+    rabbitmq_host     => '10.0.0.58',
+    subscriptions     => 'sensu-test',
     rabbitmq_ssl_private_key => "puppet:///modules/profile/ssl_certs/client/key.pem",
     rabbitmq_ssl_cert_chain  => "puppet:///modules/profile/ssl_certs/client/cert.pem",
-      redis_host        => '10.0.0.58',
+    redis_host        => '10.0.0.58',
   }
 }
