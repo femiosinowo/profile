ce175ca3f4aba9c9b658469eeb251bd8379a8314
Femi From Dell <Femi@paosin.com>
2016-04-13 15:36:12 -0400

    added some stuff on clients sensu

--- manifests/sensu_client.pp
+++ manifests/sensu_client.pp
@@ -1,8 +1,10 @@
 class profile::sensu_client(){
 
-     class { 'sensu':
-     rabbitmq_password  => 'correct-horse-battery-staple',
-     rabbitmq_host      => 'sensu.paosin.local',
-     subscriptions      => 'sensu-test',
-   }
+  class { 'sensu':
+    rabbitmq_password => 'sensu',
+    rabbitmq_host => 'sensu.paosin.local',
+    subscriptions => 'sensu-test',
+    rabbitmq_ssl_private_key => "puppet:///modules/profile/ssl_certs/client/key.pem",
+    rabbitmq_ssl_cert_chain  => "puppet:///modules/profile/ssl_certs/client/cert.pem",
+  }
 }
