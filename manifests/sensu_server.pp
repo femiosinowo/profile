01ec14c673e25dde10ef82cf7f70fe2b85efbeb7
Femi From Dell <Femi@paosin.com>
2016-04-13 10:20:10 -0400

    added ssl true

--- manifests/sensu_server.pp
+++ manifests/sensu_server.pp
@@ -11,7 +11,7 @@ class profile::sensu_server () {
     ssl_key    => '/etc/rabbitmq/ssl//key.pem',
     ssl_cert   => '/etc/rabbitmq/ssl//cert.pem',
     ssl_cacert => '/etc/rabbitmq/ssl//cacert.pem',
-    ssl        => false,
+    ssl        => true,
   } ->
   rabbitmq_vhost { '/sensu': } ->
   rabbitmq_user { 'sensu': password => 'password' } ->
