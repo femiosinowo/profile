645eaa5113addd13d3e33dfeec46170436e2299f
Femi From Dell <Femi@paosin.com>
2016-04-13 08:54:49 -0400

    added erlang

--- manifests/sensu_server.pp
+++ manifests/sensu_server.pp
@@ -1,34 +1,26 @@
 class profile::sensu_server () {
-  
-  #include '::rabbitmq'
-  
+  include 'erlang'
+
   file { '/etc/rabbitmq/ssl/cacert.pem': source => 'puppet:///modules/profiles/puppet/ssl_certs/sensu_ca/cacert.pem', }
 
   file { '/etc/rabbitmq/ssl/cert.pem': source => 'puppet:///modules/profiles/puppet/ssl_certs/server/cert.pem', }
 
   file { '/etc/rabbitmq/ssl/key.pem': source => 'puppet:///modules/profiles/puppet/ssl_certs/server/key.pem', }
-
+  class { 'erlang': epel_enable => true } ->
   class { 'rabbitmq':
     ssl_key    => '/etc/rabbitmq/ssl//key.pem',
     ssl_cert   => '/etc/rabbitmq/ssl//cert.pem',
     ssl_cacert => '/etc/rabbitmq/ssl//cacert.pem',
-    ssl        => true, 
-  }
-
-  rabbitmq_vhost { '/sensu': }
-
-  rabbitmq_user { 'sensu': password => 'password' }
- 
+    ssl        => true,
+  } ->
+  rabbitmq_vhost { '/sensu': } ->
+  rabbitmq_user { 'sensu': password => 'password' } ->
   rabbitmq_user_permissions { 'sensu@/sensu':
     configure_permission => '.*',
     read_permission      => '.*',
     write_permission     => '.*',
-  }
-
-  class { 'redis':
-    bind => '127.0.0.1';
-  }
-
+  } ->
+  class { 'redis': bind => '127.0.0.1'; } ->
   class { 'sensu':
     server                   => true,
     rabbitmq_password        => 'password',
