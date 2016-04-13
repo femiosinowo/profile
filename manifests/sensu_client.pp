0daa36b923cdbb121191fa2ef67343fef00b782e
Femi From Dell <Femi@paosin.com>
2016-04-13 15:55:46 -0400

    apihost sensu

--- manifests/sensu_client.pp
+++ manifests/sensu_client.pp
@@ -5,9 +5,10 @@ class profile::sensu_client () {
     rabbitmq_user     => 'sensu',
     rabbitmq_password => 'sensu',
     rabbitmq_host     => '10.0.0.58',
-    subscriptions     => 'sensu-test',
+    subscriptions     => 'base',
     rabbitmq_ssl_private_key => "puppet:///modules/profile/ssl_certs/client/key.pem",
     rabbitmq_ssl_cert_chain  => "puppet:///modules/profile/ssl_certs/client/cert.pem",
     redis_host        => '10.0.0.58',
+    api_host          => '10.0.0.58',
   }
 }
