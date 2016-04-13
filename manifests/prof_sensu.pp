d03040d3e2174a7881cafbbb0cbec16e09ff7a4b
Femi From Dell <Femi@paosin.com>
2016-04-12 23:21:33 -0400

    stuffs

--- manifests/prof_sensu.pp
+++ manifests/prof_sensu.pp
@@ -1,24 +1,24 @@
 class profile::prof_sensu () {
   
-#   class { 'sensu':
-#    rabbitmq_password => 'correct-horse-battery-staple',
-#    server            => true,
-#    api               => true,
-#    plugins           => [
-#      'puppet:///data/sensu/plugins/ntp.rb',
-#      'puppet:///data/sensu/plugins/postfix.rb'
-#    ]
-#  }
-#
-#  sensu::handler { 'default':
-#    command => 'mail -s \'sensu alert\' femi@paosin.local',
-#  }
-#
-#  sensu::check { 'check_ntp':
-#    command     => 'PATH=$PATH:/usr/lib/nagios/plugins check_ntp_time -H pool.ntp.org -w 30 -c 60',
-#    handlers    => 'default',
-#    subscribers => 'sensu-test'
-#  }
+   class { 'sensu':
+    rabbitmq_password => 'correct-horse-battery-staple',
+    server            => true,
+    api               => true,
+    plugins           => [
+      'puppet:///data/sensu/plugins/ntp.rb',
+      'puppet:///data/sensu/plugins/postfix.rb'
+    ]
+  }
+
+  sensu::handler { 'default':
+    command => 'mail -s \'sensu alert\' femi@paosin.local',
+  }
+
+  sensu::check { 'check_ntp':
+    command     => 'PATH=$PATH:/usr/lib/nagios/plugins check_ntp_time -H pool.ntp.org -w 30 -c 60',
+    handlers    => 'default',
+    subscribers => 'sensu-test'
+  }
   
   
   
--- manifests/sensu_server.pp
+++ manifests/sensu_server.pp
@@ -6,10 +6,10 @@ class profile::sensu_server () {
   file { '/etc/rabbitmq/ssl/key.pem': source => 'puppet:///modules/profiles/puppet/ssl_certs/server/key.pem', }
 
   class { 'rabbitmq':
-     ssl_key => '/etc/rabbitmq/ssl//key.pem',
-     ssl_cert => '/etc/rabbitmq/ssl//cert.pem',
-     ssl_cacert => '/etc/rabbitmq/ssl//cacert.pem',
-    ssl => true,
+    ssl_key    => '/etc/rabbitmq/ssl//key.pem',
+    ssl_cert   => '/etc/rabbitmq/ssl//cert.pem',
+    ssl_cacert => '/etc/rabbitmq/ssl//cacert.pem',
+    ssl        => true,
   }
 
   rabbitmq_vhost { '/sensu': }
@@ -27,17 +27,17 @@ class profile::sensu_server () {
   }
 
   class { 'sensu':
-    server            => true,
-    purge_config      => true,
-    rabbitmq_password => 'password',
-     rabbitmq_ssl_private_key => "puppet:///modules/profiles/puppet/ssl_certs/client/key.pem",
-     rabbitmq_ssl_cert_chain => "puppet:///modules/profiles/puppet/ssl_certs/client/cert.pem",
-    rabbitmq_host     => 'sensu.paosin.local',
-    subscriptions     => 'sensu-test',
+    server                   => true,
+    purge_config             => true,
+    rabbitmq_password        => 'password',
+    rabbitmq_ssl_private_key => "puppet:///modules/profiles/puppet/ssl_certs/client/key.pem",
+    rabbitmq_ssl_cert_chain  => "puppet:///modules/profiles/puppet/ssl_certs/client/cert.pem",
+    rabbitmq_host            => 'sensu.paosin.local',
+    subscriptions            => 'sensu-test',
   }
 
   firewall { '101 allow 80,3306,443':
-    dport  => [3000, 4567,5672],
+    dport  => [3000, 4567, 5672],
     proto  => tcp,
     action => accept,
   }
