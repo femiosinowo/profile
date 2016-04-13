e49b5bfff5749a54fc6c890aa00dd43370cf5a2e
Femi From Dell <Femi@paosin.com>
2016-04-13 15:48:03 -0400

    corections

--- manifests/sensu_client.pp
+++ manifests/sensu_client.pp
@@ -2,9 +2,10 @@ class profile::sensu_client(){
 
   class { 'sensu':
     rabbitmq_password => 'sensu',
-    rabbitmq_host => 'sensu.paosin.local',
+    rabbitmq_host => '10.0.0.58',
     subscriptions => 'sensu-test',
     rabbitmq_ssl_private_key => "puppet:///modules/profile/ssl_certs/client/key.pem",
     rabbitmq_ssl_cert_chain  => "puppet:///modules/profile/ssl_certs/client/cert.pem",
+      redis_host        => '10.0.0.58',
   }
 }
