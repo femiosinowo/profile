4154581c9fd01e03c59aa396c8faccb70adbd719
Femi From Dell <Femi@paosin.com>
2016-04-13 16:18:59 -0400

    changed ip

--- manifests/sensu_client.pp
+++ manifests/sensu_client.pp
@@ -1,15 +1,15 @@
 class profile::sensu_client () {
   class { '::sensu':
-    server            => false,
-    api               => true,
-    rabbitmq_user     => 'sensu',
-    rabbitmq_password => 'sensu',
-    rabbitmq_host     => '10.0.0.58',
-    subscriptions     => 'base',
+    server     => false,
+    api        => true,
+    rabbitmq_user            => 'sensu',
+    rabbitmq_password        => 'sensu',
+    subscriptions            => 'base',
     rabbitmq_ssl_private_key => "puppet:///modules/profile/ssl_certs/client/key.pem",
     rabbitmq_ssl_cert_chain  => "puppet:///modules/profile/ssl_certs/client/cert.pem",
-    redis_host        => '10.0.0.58',
-    redis_password => 'sensu',
-    api_host          => '10.0.0.58',
+    redis_host => '10.0.0.57',
+    redis_password           => 'sensu',
+    api_host   => '10.0.0.57',
+    rabbitmq_host            => '10.0.0.57',
   }
 }
