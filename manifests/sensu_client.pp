9fffdeb978d2762576a290e0d4064df712424b32
femio <femio@DESKTOP-7T6QEFN>
2016-04-14 16:21:08 -0400

    same removed ssl certs

--- manifests/sensu_client.pp
+++ manifests/sensu_client.pp
@@ -6,8 +6,8 @@ class profile::sensu_client ($sensu_server_ip = hiera('sensu::server_ip')) {
     rabbitmq_password        => 'sensu',
     rabbitmq_vhost           => '/sensu',
     subscriptions            => 'base',
-    //rabbitmq_ssl_private_key => "puppet:///modules/profile/ssl_certs/client/key.pem",
-    //rabbitmq_ssl_cert_chain  => "puppet:///modules/profile/ssl_certs/client/cert.pem",
+#    rabbitmq_ssl_private_key => "puppet:///modules/profile/ssl_certs/client/key.pem",
+#    rabbitmq_ssl_cert_chain  => "puppet:///modules/profile/ssl_certs/client/cert.pem",
 
     redis_password           => 'sensu',
     redis_host               => $sensu_server_ip,
