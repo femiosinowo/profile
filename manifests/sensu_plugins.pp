b636281b2ca0f9f07489bb0712d7035eb33d7924
femio <femio@DESKTOP-7T6QEFN>
2016-04-15 14:21:33 -0400

    added some checks

--- manifests/sensu_plugins.pp
+++ manifests/sensu_plugins.pp
@@ -1,26 +1,23 @@
 class profile::sensu_plugins () {
   file { '/opt/sensu-plugins': ensure => directory, }
 
-  staging::deploy { 'sensu-community-plugins.tar.gz':
-    source  => 'https://github.com/sensu/sensu-community-plugins/archive/master.tar.gz',
-    target  => '/opt/sensu-plugins',
-    require => File['/opt/sensu-plugins'],
-  }
-
   sensu::handler { 'default': command => 'mail -s \'sensu alert\' femi@paosin.local', }
 
-  sensu::check { 'check_cron':
-    command     => '/opt/sensu-plugins/sensu-community-plugins-master/plugins/processes/check-procs.rb -p crond -C   1',
+  package { 'sensu-plugins-cpu-checks':
+    ensure   => '0.0.1',
+    provider => sensu_gem,
+  }
+
+  package { 'sensu-plugins-disk-checks':
+    ensure   => '0.0.1',
+    provider => sensu_gem,
+  }
+
+  sensu::check { 'check_disk':
+    command     => 'check-disk-usage.rb -w 80 -c 90',
     handlers    => 'default',
     subscribers => 'base',
-    require     => Staging::Deploy['sensu-community-plugins.tar.gz'],
-  }
-  
-  sensu::check { 'check_disk':
-    command => '/opt/sensu-plugins/sensu-community-plugins-master/plugins/system/check-disk.rb',
-    handlers => 'default',
-    subscribers => 'base',
-    require => Staging::Deploy['sensu-community-plugins.tar.gz'],
+    require     => Package['sensu-plugins-cpu-checks'],
   }
 
 }
