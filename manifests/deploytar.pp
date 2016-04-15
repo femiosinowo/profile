6b58c8c0eaaa766907d8f2b5b762ff1a939bb8a8
femio <femio@DESKTOP-7T6QEFN>
2016-04-15 12:54:49 -0400

    added some stuff

--- manifests/deploytar.pp
+++ manifests/deploytar.pp
@@ -1,11 +1,18 @@
 class profile::deploytar ($source = '', $target = '') {
   include wget
 
-  wget::fetch { "download some tar file":
+  wget::fetch { "download_some_tar_file":
     source      => $source,
     destination => $target,
     timeout     => 0,
     verbose     => false,
+  }
+
+  exec { "untarit":
+    command => 'tar -xvf ',
+    cwd     => $target,
+    path    => ["/bin", "/usr/sbin"],
+    require => Exec['download_some_tar_file']
   }
 
 }
--- manifests/sensu_plugins.pp
+++ manifests/sensu_plugins.pp
@@ -1,17 +1,19 @@
 class profile::sensu_plugins () {
+  file { '/opt/sensu-plugins': ensure => directory, }
 
-
-
-  file { '/opt/sensu-plugins':
-    ensure  => directory 
+  staging::deploy { 'sensu-community-plugins.tar.gz':
+    source  => 'https://github.com/sensu/sensu-community-plugins/archive/master.tar.gz',
+    target  => '/opt/sensu-plugins',
+    require => File['/opt/sensu-plugins'],
   }
 
-  archive { 'sensu-plugins':
-    ensure => present,
-    url    => 'https://github.com/sensu/sensu-community-plugins/archive/master.tar.gz',
-    target => '/opt/sensu-plugins',
-    require => File['/opt/sensu-plugins']
+  sensu::handler { 'default': command => 'mail -s \'sensu alert\' femi@paosin.local', }
+
+  sensu::check { 'check_cron':
+    command     => '/opt/sensu-plugins/sensu-community-plugins-master/plugins/processes/check-procs.rb -p crond -C   1',
+    handlers    => 'default',
+    subscribers => 'base',
+    require     => Staging::Deploy['sensu-community-plugins.tar.gz'],
   }
 
- 
 }
