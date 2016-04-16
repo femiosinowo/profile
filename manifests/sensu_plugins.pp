39c2cd0f065bd0245c6e62abc4dd6521539e53dd
femio <femio@DESKTOP-7T6QEFN>
2016-04-16 12:51:45 -0400

    added memory check and path

--- manifests/sensu_plugins.pp
+++ manifests/sensu_plugins.pp
@@ -1,18 +1,6 @@
 class profile::sensu_plugins () {
-  package { 'centos-release-SCL':
-    ensure   => latest,
-    provider => yum,
-  }
-
-  package { 'ruby193':
-    ensure   => latest,
-    provider => yum,
-    require  => Package['centos-release-SCL']
-  }
-
-  # profiled::script { 'ruby193.sh': content => 'source /opt/rh/ruby193/enable' }
-
-  file { '/opt/sensu-plugins': ensure => directory, }
+  Exec {
+    path => '/usr/bin:/opt/sensu/embedded/bin/', }
 
   sensu::handler { 'default': command => 'mail -s \'sensu alert\' femi@paosin.local', }
 
@@ -31,16 +19,35 @@ class profile::sensu_plugins () {
     provider => sensu_gem,
   }
 
-  package { 'redphone':
+  package { 'sensu-plugins-memory-checks':
     ensure   => 'installed',
     provider => sensu_gem,
   }
 
   sensu::check { 'check_disk':
-    command     => '/opt/sensu/embedded/bin/check-disk-usage.rb -w 80 -c 90',
+    command     => 'check-disk-usage.rb -w 80 -c 90',
     handlers    => 'default',
     subscribers => 'base',
     require     => Package['sensu-plugins-cpu-checks'],
   }
-
+  sensu::check { 'check_memory':
+    command     => 'check-memory-percent.rb',
+    handlers    => 'default',
+    subscribers => 'base',
+    require     => Package['sensu-plugins-memory-checks'],
+  }
+  #  package { 'centos-release-SCL':
+  #    ensure   => latest,
+  #    provider => yum,
+  #  }
+  #
+  #  package { 'ruby193':
+  #    ensure   => latest,
+  #    provider => yum,
+  #    require  => Package['centos-release-SCL']
+  #  }
+  #
+  #  # profiled::script { 'ruby193.sh': content => 'source /opt/rh/ruby193/enable' }
+  #
+  #  file { '/opt/sensu-plugins': ensure => directory, }
 }
