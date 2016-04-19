c0df1aeb7a85c71015c0cf957ef6baffba39ce87
femio <femio@DESKTOP-7T6QEFN>
2016-04-19 01:02:41 -0400

    some edits

--- manifests/plex.pp
+++ manifests/plex.pp
@@ -10,17 +10,6 @@ class profile::plex () {
     owner  => 'plex',
     group  => 'plex'
   } ->
-  class { 'plexmediaserver':
-    plex_user              => 'plex',
-    plex_media_server_home => '/plex',
-    plex_media_server_application_support_dir => '/opt/plex/Library/Application Support',
-  #    plex_media_server_max_plugin_procs        => '7',
-  #    plex_media_server_max_stack_size          => '20000',
-  #    plex_media_server_max_lock_mem            => '4000',
-  #    plex_media_server_max_open_files          => '1024',
-  #    plex_media_server_tmpdir => '/var/tmp',
-  }
-
   class { 'samba::server':
     workgroup     => 'plex',
     server_string => "Movies",
