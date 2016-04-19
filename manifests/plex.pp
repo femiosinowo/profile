f56312d848ecfd31aaeba935b4e451af789882e0
femio <femio@DESKTOP-7T6QEFN>
2016-04-19 00:51:23 -0400

    added firewall rules

--- manifests/plex.pp
+++ manifests/plex.pp
@@ -20,28 +20,38 @@ class profile::plex () {
   #    plex_media_server_tmpdir => '/var/tmp',
   }
 
- 
+  class { 'samba::server':
+    workgroup     => 'plex',
+    server_string => "Movies",
+    interfaces    => "eth0 lo",
+    security      => 'share'
+  }
 
-   class { 'samba::server':
-      workgroup     => 'plex',
-      server_string => "Movies",
-      interfaces    => "eth0 lo",
-      security      => 'share'
-    }
-  
-    samba::server::share { 'PlexMovies':
-      comment              => 'PlexMovies',
-      path                 => '/plex',
-      # # guest_only           => true,
-      guest_ok             => true,
-      # guest_account        => "guest",
-      browsable            => true,
-      create_mask          => 0777,
-      force_create_mask    => 0777,
-      directory_mask       => 0777,
-      force_directory_mask => 0777,
+  samba::server::share { 'PlexMovies':
+    comment              => 'PlexMovies',
+    path                 => '/plex',
+    # # guest_only           => true,
+    guest_ok             => true,
+    # guest_account        => "guest",
+    browsable            => true,
+    create_mask          => 0777,
+    force_create_mask    => 0777,
+    directory_mask       => 0777,
+    force_directory_mask => 0777,
   #  #    force_group          => 'group',
   #  #    force_user           => 'user',
   #  #    copy                 => 'some-other-share',
-    }
+  }
+
+  firewall { '120 allow puppet stuff':
+    dport  => [139, 445, 3005, 8324, 32469, 32400],
+    proto  => tcp,
+    action => accept,
+  }
+
+  firewall { '121 allow puppet stuff':
+    dport  => [137, 138, 1900, 5353, 32410],
+    proto  => udp,
+    action => accept,
+  }
 }
