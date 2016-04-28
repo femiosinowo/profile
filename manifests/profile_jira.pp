d7c52765437450e1fe25a5fee32455410d5f6234
Femi From Dell <Femi@paosin.com>
2016-04-27 21:45:57 -0400

    added javahome

--- manifests/profile_jira.pp
+++ manifests/profile_jira.pp
@@ -1,7 +1,7 @@
 class profile::profile_jira () {
   include jdk_oracle
 
-  class { 'jira': javahome => '/opt/java', }
+  class { 'jira': javahome => '/opt/java_home/', }
 
   firewall { '120 allow puppet stuff':
     dport  => [8080],
