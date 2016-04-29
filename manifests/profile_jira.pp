7e822e90ab745cee4f66e8ef34f98b6aa799c2b6
Femi From Dell <Femi@paosin.com>
2016-04-28 21:54:14 -0400

    added postgresql

--- manifests/profile_jira.pp
+++ manifests/profile_jira.pp
@@ -1,7 +1,18 @@
 class profile::profile_jira () {
   include jdk_oracle
 
-  class { 'jira': javahome => '/opt/java_home/', }
+class { 'postgresql::server': }
+
+postgresql::server::db { 'jira':
+  user     => 'jiraadm',
+  password => postgresql_password('jiraadm', 'mypassword'),
+}
+
+  class { 'jira':
+    javahome => '/opt/java_home/',
+    db       => 'postgresql',
+    dbuser   => 'jiraadm',
+  }
 
   firewall { '120 allow puppet stuff':
     dport  => [8080],
