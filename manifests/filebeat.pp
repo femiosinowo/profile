6ed8dca562d40fff5cddb53b9eea60ee3a1053ec
Femi From Dell <Femi@paosin.com>
2016-04-30 00:10:16 -0400

    correction

--- manifests/filebeat.pp
+++ manifests/filebeat.pp
@@ -2,7 +2,7 @@ class profile::filebeat ($elkstack_host = hiera('elk::host'),) {
   class { '::filebeat':
     outputs     => {
       'logstash' => {
-        'hosts'       => ["${elkstack_host}.5044"],
+        'hosts'       => ["${elkstack_host}:5044"],
         'loadbalance' => true,
         # 'tls'
       }
