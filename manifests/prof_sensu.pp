9b510376ad2694bb7a9b32504c8a31aed328867f
Femi From Dell <Femi@paosin.com>
2016-05-03 00:25:47 -0400

    asdrf

--- manifests/prof_sensu.pp
+++ manifests/prof_sensu.pp
@@ -14,11 +14,7 @@ class profile::prof_sensu () {
     command => 'mail -s \'sensu alert\' femi@paosin.local',
   }
 
-  sensu::check { 'check_ntp':
-    command     => 'PATH=$PATH:/usr/lib/nagios/plugins check_ntp_time -H pool.ntp.org -w 30 -c 60',
-    handlers    => 'default',
-    subscribers => 'sensu-test'
-  }
+ 
   
   
   
